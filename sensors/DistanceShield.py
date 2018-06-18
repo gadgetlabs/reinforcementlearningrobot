03538f5f885f805f0c507325d2c0cc4294b6f25c
Assessment <assessment@ORTIZ16s-Air.lan>
2018-06-18 22:04:25 +0100

    Refactor with https://github.com/johnbryanmoore/VL53L0X_rasp_python

--- sensors/DistanceShield.py
+++ sensors/DistanceShield.py
@@ -8,36 +8,43 @@ class DistanceShield:
         self.NUMBER_OF_TOF_SENSORS = 0
         self.BASE_I2C_ADDRESS = 0x29
         self.NEW_I2C_ADDRESS_START = 0x30
-        self.VL53L0X_REG_I2C_SLAVE_DEVICE_ADDRESS = 0x008a
 
     def number_sensors(self, number_sensors):
         self.NUMBER_OF_TOF_SENSORS = number_sensors
 
     def reset_all(self):
         self.__off_all()
-        time.sleep(10)
+        time.sleep(3)
         self.__on_all()
 
     def change_addresses(self, bus):
 
         self.__off_all()
 
+        self.sensors = list()
         for x in range(self.NUMBER_OF_TOF_SENSORS):
+
+            tof = VL53L0X.VL53L0X(address=self.NEW_I2C_ADDRESS_START + x)
+            self.sensors.append(tof)
+
             # Bring up sensor x (not same as x - 1!)
             explorerhat.output[x].on()
             # Going with 0x30 to 0x3F is probably OK.
 
-            self.set_device_address(bus, self.NEW_I2C_ADDRESS_START + x)
+        time.sleep(3)
 
-    def set_device_address(self, bus, new_addr):
-        # Line from ST API (vl53l0x_api.c)
-        # Status = VL53L0X_WrByte(Dev, VL53L0X_REG_I2C_SLAVE_DEVICE_ADDRESS, DeviceAddress / 2);
-        bus.write_byte_data(self.BASE_I2C_ADDRESS, self.VL53L0X_REG_I2C_SLAVE_DEVICE_ADDRESS, new_addr)
+    def start_ranging(self, bus):
 
-    def get_device_mode(self, bus):
+        for x in range(self.NUMBER_OF_TOF_SENSORS):
+            self.sensors[x].start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
 
-
-        self.device_mode =
+        distances = list()
+        for count in range(1, 101):
+            for x in range(self.NUMBER_OF_TOF_SENSORS):
+                distance = self.sensors[x].get_distance()
+                distances.append(distance)
+            print ", ".join([y for y in distances])
+            distances = list()
 
 
     def __off_all(self):
@@ -47,15 +54,5 @@ class DistanceShield:
     def __on_all(self):
         for x in range(self.NUMBER_OF_TOF_SENSORS):
             explorerhat.output[x].on()
-
-        VL53L0X_GETPARAMETERFIELD(Dev, DeviceMode, *pDeviceMode);
-
-        # define VL53L0X_GETPARAMETERFIELD(Dev, field, variable) \
-        variable = PALDevDataGet(Dev, CurrentParameters).field
-
-
-        VL53L0X_Error
-        VL53L0X_StartMeasurement(VL53L0X_DEV
-        Dev)
 
 
